8c6de345c422dca4bbac9a0ccb004367e0f35043
fquach <129032141+noktalis@users.noreply.github.com>
2023-12-31 23:26:20 -0800

    migrated global styling assets

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,12 @@
-import '@/styles/globals.css'
+/**
+ * Followed from:
+ * https://nextjs.org/learn-pages-router/basics/assets-metadata-css/global-styles
+ * 
+ * This file exists to apply global.scss and other global stylesheets across the app
+ */
+
+import '/styles/global.scss';
+import '/styles/root.scss';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
