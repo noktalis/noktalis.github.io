5347f5362285f02821a8bff29768c894a10ab1d0
noktalis <129032141+noktalis@users.noreply.github.com>
2024-01-20 16:24:07 -0800

    added rosmontis highlight entry

--- pages/ak/story.js
+++ pages/ak/story.js
@@ -47,13 +47,51 @@ function Content() {
 					summaryText="This lovely speech at the end of the world [M8-8 After]"
 					
 					details={<>
-						<img src='/images/ak/story/priestess_m8-8/1.png'/>
-						<img src='/images/ak/story/priestess_m8-8/2.png'/>
-						<img src='/images/ak/story/priestess_m8-8/3.png'/>
-						<img src='/images/ak/story/priestess_m8-8/4.png'/>
-						<img src='/images/ak/story/priestess_m8-8/5.png'/>
-						<img src='/images/ak/story/priestess_m8-8/6.png'/>
-						<img src='/images/ak/story/priestess_m8-8/7.png'/>
+						<img src='/images/ak/story/priestess_m8-8/1.png'
+							alt="Priestess: I never thought I would be the one who didn't want to let go."
+							title="Priestess: I never thought I would be the one who didn't want to let go."/>
+						<img src='/images/ak/story/priestess_m8-8/2.png'
+							alt="Priestess, holding Doctor's hand as they lay in a sarcophagus: But I have to do this. This is the only way you'll get to live."
+							title="Priestess: But I have to do this. This is the only way you'll get to live."/>
+						<img src='/images/ak/story/priestess_m8-8/3.png'
+							alt="Priestess: ...Oh, Doctor... With this, we won't ever get to meet again."
+							title="Priestess: ...Oh, Doctor... With this, we won't ever get to meet again."/>
+						<img src='/images/ak/story/priestess_m8-8/4.png'
+							alt="Priestess: I can't. I can't accept this. I won't give up."
+							title="Priestess: I can't. I can't accept this. I won't give up."/>
+						<img src='/images/ak/story/priestess_m8-8/5.png'
+							alt="Priestess: Doctor, the connection between us will transcend even time and space. I am sure of it."
+							title="Priestess: Doctor, the connection between us will transcend even time and space. I am sure of it."/>
+						<img src='/images/ak/story/priestess_m8-8/6.png'
+							alt="Priestess: Even if the ocean completely boils away and the atmosphere vanishes, even if all the moons in the sky are pulled into the vortex of our planet's gravity, and even if the sun continues to mercilessly expand, eventually swallowing up all its children until none can be heard..."
+							title="The most Yearning&trade; way to say you'll meet again in the afterlife"/>
+						<img src='/images/ak/story/priestess_m8-8/7.png'
+							alt="Priestess: I am sure we will meet again. At the far end of our civilization, once adorned by darkness and the brilliance of the stars up above, we will meet again."
+							title="Priestess: I am sure we will meet again. At the far end of our civilization, once adorned by darkness and the brilliance of the stars up above, we will meet again."/>
+						<hr/>
+						<p title='Afterthoughts'>
+							This was just??? So Yearning&trade;??? I'm such a sucker for angst AND tragedy AND partings specifically at the end of the world...
+						</p>
+					</>}>
+				</DetailsListEntry>
+				<DetailsListEntry
+					title="Rosmontis' Choice"
+					summaryText="Rosmontis on child soldiers [7-6 After]"
+					
+					details={<>
+						<img src='/images/ak/story/rosmontis_7-6/1.png'/>
+						<img src='/images/ak/story/rosmontis_7-6/2.png'/>
+						<img src='/images/ak/story/rosmontis_7-6/3.png'/>
+						<img src='/images/ak/story/rosmontis_7-6/4.png'/>
+						<img src='/images/ak/story/rosmontis_7-6/5.png'/>
+						<img src='/images/ak/story/rosmontis_7-6/6.png'/>
+						<img src='/images/ak/story/rosmontis_7-6/7.png'/>
+						<img src='/images/ak/story/rosmontis_7-6/8.png'/>
+						<img src='/images/ak/story/rosmontis_7-6/9.png'/>
+						<hr/>
+						<p>
+							
+						</p>
 					</>}>
 				</DetailsListEntry>
 			</DetailsList>
