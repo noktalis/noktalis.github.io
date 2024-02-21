e54f54c79aedc54bf3b85c3ea4fb533078816c2a
noktalis <129032141+noktalis@users.noreply.github.com>
2024-02-20 23:16:29 -0800

    leisure time pack added to pack json for src link testing

--- components/ak/stickergallery.js
+++ components/ak/stickergallery.js
@@ -8,39 +8,90 @@ import { ThemeContext } from "../pageFormat/ThemeContext";
  */
 export default function AKStickerGallery(data){
 	let pack = data['pack'];
+	let cnDate;
+	// let enDate;
 
-	let cnDate = dateStringReformat(pack["cn-date"]);
-	
+	try {
+		cnDate = dateStringReformat(pack["cn-date"]);
+		// enDate = dateStringReformat(pack["en-date"]);
+		// console.log(cnDate);
+		console.log("Dates set");
+	} catch (error) {
+		console.log("Could not set dates");
+	}
 	
 	
 	
 
-	// cnDateStr = pack["cn-date"]
 
 	
 	
 	return (
 		<>
-			{
-				data ? <p>
-							Released to CN: <a href='' target="_blank">{cnDate ? cnDate : ""}</a>
-						</p>
-				:
-				<></>
-			}
+		{ pack ? 	
+			<>
+				<CNDate src={pack["cn-src"]} dateStr={cnDate}/>
+				{/* <ENDate src="" dateStr={enDate}/> */}
+			</>
+			: <></>
+		}
 		</>
 	); // end return
 } // end Component
 
+function ENDate({dateStr, src}) {
+	if (src) {
+		return (
+			<p>
+				Released to EN: <a href={src} target="_blank">{dateStr}</a>
+			</p>
+		);
+	} else if (dateStr) {
+		return (
+			<p>
+				Released to EN: {dateStr}
+			</p>
+		);
+	} else {
+		return (
+			<p>
+				Released to EN: Unknown
+			</p>
+		);
+	}
+}
+
+function CNDate({dateStr, src}) {
+	if (src) {
+		return (
+			<p>
+				Released to CN: <a href={src} target="_blank">{dateStr}</a>
+			</p>
+		);
+	} else if (dateStr) {
+		return (
+			<p>
+				Released to CN: {dateStr}
+			</p>
+		);
+	} else {
+		return (
+			<p>
+				Released to CN: Unknown
+			</p>
+		);
+	}
+}
+
 /**
- * Converts a data string from YYYY-MM-DD (ISO) format to Mon DD YYYY format
+ * Converts a data string from YYYY-MM-DD (ISO) format to DD Mon YYYY format
  * @param {String} string - a date in ISO format
  * 
- * @returns date string in Mon DD YYYY format
+ * @returns date string in DD Mon YYYY format
  */
 function dateStringReformat(string) {
 	let date = new Date(string);
-	let newStr = date.toDateString().substring(4);	// Removes day of week at start of string
+	let newStr = date.toUTCString().substring(5,16);
 
 	return newStr;
 }
