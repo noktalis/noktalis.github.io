ee0554900cd888d930d07f28c7e0f1b2c8ecb6b6
noktalis <129032141+noktalis@users.noreply.github.com>
2025-01-25 00:25:17 -0800

    minor restructuring of ak sticker gallery, prep for visual update

--- pages/ak/stickers/gallery.js
+++ pages/ak/stickers/gallery.js
@@ -5,7 +5,8 @@ import { FandomContext } from '@/components/pageFormat/FandomContext';
 import React, { useState, useEffect, useContext } from 'react';
 import AKStickerGallery from '@/components/ak/stickergallery';
 import ReactPaginate from 'react-paginate';
-import style from "../../styles/modules/pagination.module.scss";
+import style from "@/styles/modules/pagination.module.scss";
+import tabstyle from "@/styles/modules/tabs.module.scss";
 import { ToTop } from '@/components/pageFormat/main';
 
 //https://www.youtube.com/watch?v=HANSMtDy508
@@ -48,8 +49,20 @@ function Content() {
 			themeClass = style.ri;
 	}
 
+	// set year of gallery display
+	const [year, setYear] = useState(0)		// 0: all, 1: 2019, 2: 2020, etc.
+	function updateYear(i){
+		// update css
+
+		// change data
+		// setYear(i)
+		// console.log(year)
+	}
+
 	// paginate sticker galleries
-	const [packs, setPacks] = useState([]);
+	const [data, setData] = useState([]) 
+	const [ydata, setYData] = useState([])	// 0: 2019, 1: 2020, etc.
+	const [packs, setPacks] = useState([])
 	const [pageNumber, setPageNumber] = useState(0);
 
 	const itemsPerPage = 5;
@@ -68,8 +81,16 @@ function Content() {
 			/* Fetch request */
 			const response = await fetch("https://noktalis.github.io/ak-stickers/info.json");
 			const obj = await response.json();
-			console.log(obj);
+			setData(obj);
 			setPacks(obj);
+
+			const y2019 = obj.slice(-17)
+			const y2020 = obj.slice(-37, -17)
+
+			// console.log(y2020)
+			
+			setYData([y2019,y2020])
+			// console.log(data)
 		}
 		fetchData()
 		.catch(console.error);
@@ -80,7 +101,18 @@ function Content() {
 		<div>
 			<h1 style={{textAlign:"center"}}>Arknights Sticker Archive</h1>
 			<h4 style={{textAlign:"center"}}>Ordered by CN release date | Newest first</h4>
-			<p style={{textAlign:"center"}}>Shoutout to Xue for also translating all <br/>the Chinese text and helping fetch sticker sheets & CN release dates from Weibo!</p>
+
+			{/* <Tabs>
+				<Tab select={true}>All</Tab>
+				<Tab>2019</Tab>
+				<Tab>2020</Tab>
+				<Tab disable={true}>2021</Tab>
+				<Tab disable={true}>2022</Tab>
+				<Tab disable={true}>2023</Tab>
+				<Tab disable={true}>2024</Tab>
+				<Tab disable={true}>2025</Tab>
+			</Tabs> */}
+			
 			<ReactPaginate
 				previousLabel={
 					<svg width="30px" height="30px" viewBox="0 -1 25 22" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -101,9 +133,10 @@ function Content() {
 				nextLinkClassName={style.next}
 				disabledClassName={style.disabledarrow}
 				style={{textAlign:"center"}}
+				renderOnZeroPageCount={null}
 			/>
 
-			{display.map((pack) => <AKStickerGallery packData={pack}></AKStickerGallery>)}
+			{display.map((pack) => <AKStickerGallery packData={pack} key={pack["pack"]}></AKStickerGallery>)}
 
 			<div style={{textAlign:"center"}}>
 				<ToTop/>
